3a43c6b76113cd9fe78ee874a1f4c407d5031b13
estrella23soto <estrellajuarez8soto@gmail.com>
2026-09-21 20:45:10 -0600

    se agregan controller

---
+++ Controllers/QuestionsController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Models.Entities;
+using UseCases.Questions;
+using ROP;
+
+namespace Quiniela.Controllers
+{
+    [ApiController]
+    [Route("questions")]
+    public class QuestionsController(QuestionsUseCases _questions) : Controller
+    {
+        //[HasPermissionOnAction(Constants.Actions.ReadForms)]
+        [HttpGet("")]
+        public async Task<ActionResult<GenericResponse<List<Question>>>> GetAllForms()
+        {
+            var result = await _questions.GetAllQuestions.Execute("tenant-id");
+            return result
+                .ToGenericResponse()
+                .ToActionResult();
+        }
+
+    }
+}
